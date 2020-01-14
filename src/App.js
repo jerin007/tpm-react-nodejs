837d1232001fd89a7e9fe75b604dc14cc9ddbaf6
jerin007 <jerinjahan@ymail.com>
2020-01-14 17:10:58 +0600

    Modified

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from "./components/navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+// 	return (
+// 		<div className="App">
+// 		</div>
+// 	);
+// }
+
+class App extends Component {
+	state = {};
+  
+	// componentDidMount() {
+	//   const user = auth.getCurrentUser();
+	//   this.setState({ user });
+	// }
+  
+	render() {
+	  const { user } = this.state;
+  
+	  return (
+		<React.Fragment>
+		  {/* <ToastContainer /> */}
+		  <NavBar user={user} />
+		  <main className="container">
+			{/* <Switch>
+				<Route path="/register" component={RegisterForm} />
+				<Route path="/login" component={LoginForm} />
+				<Route path="/logout" component={Logout} />
+				<ProtectedRoute path="/movies/:id" component={MovieForm} />
+				<Route
+				path="/movies"
+				render={props => <Movies {...props} user={this.state.user} />}
+				/>
+				<Route path="/customers" component={Customers} />
+				<Route path="/rentals" component={Rentals} />
+				<Route path="/not-found" component={NotFound} />
+				<Redirect from="/" exact to="/movies" />
+				<Redirect to="/not-found" />
+			</Switch> */}
+		  </main>
+		</React.Fragment>
+	  );
+	}
+  }
 
 export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+// import { Link, NavLink } from "react-router-dom";
+
+const NavBar = ({ user }) => {
+	return (
+		// <nav className="navbar navbar-expand-lg navbar-light bg-light">
+		// </nav>
+		<nav className="brand-logo">
+			{/* <a href="index-2.html">
+				<b className="logo-abbr"><img src="images/logo.png" alt=""> </b>
+				<span className="logo-compact"><img src="images/logo-compact.png" alt=""></span>
+				<span className="brand-title">
+					<img src="images/logo-text.png" alt="">
+				</span>
+			</a> */}
+		</nav>
+	);
+};
+
+export default NavBar;
